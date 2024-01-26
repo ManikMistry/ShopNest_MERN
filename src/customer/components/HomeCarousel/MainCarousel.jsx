65a30e37561fd82becd75a3cb314b02e21b61e2a
Manik Mistry <mkmistryhd2019@gmail.com>
2024-01-26 23:14:30 +0530

    Working on Navigation Items

--- src/customer/components/HomeCarousel/MainCarousel.jsx
+++ src/customer/components/HomeCarousel/MainCarousel.jsx
@@ -3,7 +3,7 @@ import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
 import { MainCarouselData } from './MainCarouselData';
 
-const items = MainCarouselData.map((item)=><img className='cursor-pointer' role='presentation' alt='' src={item.image}/>)
+const items = MainCarouselData.map((item)=><img className='cursor-pointer -z-10' role='presentation' alt='' src={item.image}/>)
 
 const MainCarousel = () => (
     <AliceCarousel
