43abef7b33d6db921cbbc0b20c31d65dbaf32867
Manik Mistry <mkmistryhd2019@gmail.com>
2024-01-31 00:42:18 +0530

    added product dynamically

--- src/customer/components/Product/ProductCard.jsx
+++ src/customer/components/Product/ProductCard.jsx
@@ -1,26 +1,26 @@
 import React from "react"
 import './ProductCard.css'
 
-function ProductCard() {
+function ProductCard({product}) {
   return (
     <>
       <div className="productCard w-[15rem] m-3 transition-all cursor-pointer">
         <div className="h-[20rem]">
           <img
             className="h-full w-full object-cover object-left-top"
-            src="https://images.bewakoof.com/t1080/men-oversize-aop-shirt-18-586207-1693910939-1.jpg"
+            src={product.image}
             alt="img"
           />
         </div>
         <div className="textPart bg-white p-3">
           <div>
-            <p className="font-bold opacity-60">OFFICIAL DISNEY MERCHANDISE</p>
-            <p>Men's Black All Over Mickey Printed Oversized Shirt</p>
+            <p className="font-bold opacity-60">{product.brand}</p>
+            <p>{product.title}</p>
           </div>
           <div className="flex items-center space-x-2">
-            <p className="font-semibold">₹999</p>
-            <p className="line-through opacity-50">₹1798</p>
-            <p className="text-green-600 font-semibold">70% off</p>
+            <p className="font-semibold">{product.selling_price}</p>
+            <p className="line-through opacity-50">{product.price}</p>
+            <p className="text-green-600 font-semibold">{product.disscount}</p>
           </div>
         </div>
       </div>
